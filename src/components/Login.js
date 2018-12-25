992e612d3eb504cbc71da7bebf5dca545c964513
tadckle <tadckle@126.com>
2018-12-25 21:58:07 +0800

    Add login page skeleton.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Login.css';
+import 'semantic-ui-css/semantic.min.css';
+import PokerImage from '../images/poker.jpg';
 
 function Login(props) {
     return (
-        <div>
-            Login.
+        <div id="login">
+            <div id="loginInput">
+                <div  class="ui input focus"><input id="userName" type="text" placeholder="Enter user name ..."/></div>
+                <button id="loginBtn" class="ui primary button">Login</button><br/>
+                <div class="ui divider"></div>
+                <div class="ui centered large image">
+                    <img src={PokerImage} alt="Triple Ace"/>
+                </div>
+            </div>
         </div>
     );
 }
